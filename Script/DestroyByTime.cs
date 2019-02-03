915df3c58bc77cb1f1f70d48b6e97c77017774b9
mleech1 <marie.leech@mail.mcgill.ca>
2019-02-03 11:24:05 -0500

    Updated Code

---
+++ Script/DestroyByTime.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DestroyByTime : MonoBehaviour
+{
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if (other.tag == "boundary")
+        //{
+        //    return;
+        //}
+
+        Destroy(this.gameObject);
+    }
+}
